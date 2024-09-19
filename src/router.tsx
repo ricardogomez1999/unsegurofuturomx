b859d57dd9a2573615302c1fbec2b59e467bcd19
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2024-09-19 09:18:23 -0600

    adding lazy loading

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./views/Home";
+import { lazy, Suspense } from "react";
 import InformationLayOut from "./Layouts/InformationLayOut";
-import AboutUs from "./views/AboutUs";
-import ContactUs from "./views/ContactUs";
-import SeguroDeVida from "./views/SeguroDeVida";
-import GastosMedicos from "./views/GastosMedicos";
-import VidaMujer from "./views/VidaMujer";
-import StarDotal from "./views/StarDotal";
-import Educacion from "./views/Educacion";
-import SeguroDeAhorro from "./views/SeguroDeAhorro";
-import AvisoDePrivacidad from "./views/AvisoDePrivacidad";
 import { HelmetProvider } from "react-helmet-async";
+const AboutUs = lazy(() => import("./views/AboutUs"));
+const ContactUs = lazy(() => import("./views/ContactUs"));
+const SeguroDeVida = lazy(() => import("./views/SeguroDeVida"));
+const GastosMedicos = lazy(() => import("./views/GastosMedicos"));
+const VidaMujer = lazy(() => import("./views/VidaMujer"));
+const StarDotal = lazy(() => import("./views/StarDotal"));
+const Educacion = lazy(() => import("./views/Educacion"));
+const SeguroDeAhorro = lazy(() => import("./views/SeguroDeAhorro"));
+const AvisoDePrivacidad = lazy(() => import("./views/AvisoDePrivacidad"));
+const Home = lazy(() => import("./views/Home"));
 
 export default function Router() {
   return (
@@ -18,18 +19,85 @@ export default function Router() {
       <BrowserRouter>
         <Routes>
           <Route element={<InformationLayOut />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/aboutus" element={<AboutUs />} />
-            <Route path="/contactus" element={<ContactUs />} />
-            <Route path="/segurodevida" element={<SeguroDeVida />} />
-            <Route path="/gastosmedicos" element={<GastosMedicos />} />
-            <Route path="/vidamujer" element={<VidaMujer />} />
-            <Route path="/stardotal" element={<StarDotal />} />
-            <Route path="/educacion" element={<Educacion />} />
-            <Route path="/segurodeahorro" element={<SeguroDeAhorro />} />
+            <Route
+              path="/"
+              element={
+                <Suspense fallback="Loading...">
+                  <Home />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/aboutus"
+              element={
+                <Suspense fallback="Loading...">
+                  <AboutUs />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/contactus"
+              element={
+                <Suspense fallback="Loading...">
+                  <ContactUs />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/segurodevida"
+              element={
+                <Suspense fallback="Loading...">
+                  <SeguroDeVida />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/gastosmedicos"
+              element={
+                <Suspense fallback="Loading...">
+                  <GastosMedicos />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/vidamujer"
+              element={
+                <Suspense fallback="Loading...">
+                  <VidaMujer />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/stardotal"
+              element={
+                <Suspense fallback="Loading...">
+                  <StarDotal />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/educacion"
+              element={
+                <Suspense fallback="Loading...">
+                  <Educacion />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/segurodeahorro"
+              element={
+                <Suspense fallback="Loading...">
+                  <SeguroDeAhorro />
+                </Suspense>
+              }
+            />
             <Route
               path="/aviso-de-privacidad"
-              element={<AvisoDePrivacidad />}
+              element={
+                <Suspense fallback="Loading...">
+                  <AvisoDePrivacidad />
+                </Suspense>
+              }
             />
           </Route>
         </Routes>
